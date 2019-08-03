77ee088dbaa4f58b72e890f04b68cd2d9212a887
thephisics101 <thephisics101@gmail.com>
2019-08-03 19:18:16 +0300

    Added a Circular waveform

--- src/Components/Visualizer.tsx
+++ src/Components/Visualizer.tsx
@@ -8,6 +8,7 @@ export enum Model {
   Oscilloscope = "Oscilloscope",
   Waveform = "Waveform",
   DiffWave = "Difference Waveform",
+  WaveCircle = "Circular Waveform"
 }
 
 interface ShapeModelBase {
@@ -15,11 +16,12 @@ interface ShapeModelBase {
 }
 interface LineModel extends ShapeModelBase {
   f: "line",
-  v(i: number): number,
   y(i: number): number
 }
 interface CircleModel extends ShapeModelBase {
-  f: "circle"
+  f: "circle",
+  readonly props: {x: number, y: number},
+  r(i: number): number
 }
 
 type ShapeModel = LineModel|CircleModel;
@@ -27,9 +29,10 @@ type ShapeModel = LineModel|CircleModel;
 const map = (tData: Uint8Array, fData: Uint8Array, pFData: number[]): {[m in Model]: ShapeModel|ShapeModel[]|undefined} => {
   return {
     [Model.Empty]: undefined,
-    [Model.Oscilloscope]: {f: "line", v: i => tData[i] / 128.0, y: v => v * dim.h / 2},
-    [Model.Waveform]: {f: "line", v: i => fData[i] / 256.0, y: v => dim.h - v * dim.h},
-    [Model.DiffWave]: {f: "line", v: i => (pFData[i] - fData[i] + 128) / 128.0, y: v => v * dim.h/2}
+    [Model.Oscilloscope]: {f: "line", y: i => tData[i] / 128.0 * dim.h / 2},
+    [Model.Waveform]: {f: "line", y: i => dim.h - fData[i] / 256.0 * dim.h},
+    [Model.DiffWave]: {f: "line", y: i => (pFData[i] - fData[i] + 128) / 256.0 * dim.h},
+    [Model.WaveCircle]: {f: "circle", props: {x: dim.w/2, y: dim.h/2}, r: i => fData[i] / 256.0 * dim.h/4 + dim.h/8}
   }
 };
 
@@ -39,14 +42,15 @@ interface Props {
 }
 abstract class Visualizer extends React.Component<Props> {
   prevFData: number[] = [];
-  abstract generateLine(v: (i: number) => number, y: (v: number) => number): any;
+  abstract generateLine(m: LineModel): any;
+  abstract generateCircle(m: CircleModel): any;
 
   selectGenerator(args: ShapeModel): any {
     switch (args.f) {
       case "line":
-        return this.generateLine(args.v, args.y);
+        return this.generateLine(args);
       case "circle":
-        return null;
+        return this.generateCircle(args);
     }
     throw new Error()
   }
@@ -57,14 +61,26 @@ class SVGVisualizer extends Visualizer {
     setInterval(() => this.forceUpdate(), 1000 / framerate);
   }
 
-  generateLine(v: (i: number) => number, y: (v: number) => number): ReactNode {
+  generateLine(m: LineModel): ReactNode {
     const bufferLength = this.props.analyzer.frequencyBinCount;
     let d = "M";
     const sliceWidth = dim.w / bufferLength;
     for (let i = 0, x = 0; i < bufferLength; i++) {
-      d += `${x},${y(v(i))} `;
+      d += `${x},${m.y(i)} `;
       x += sliceWidth;
     }
+    return <path d={d} strokeWidth={1} stroke={"rgba(255, 255, 255, 0.4)"} fill="none"/>;
+  }
+  generateCircle(m: CircleModel): ReactNode {
+    const bufferLength = this.props.analyzer.frequencyBinCount;
+    let d = "M";
+    const sliceWidth = 2*Math.PI / bufferLength;
+    for (let i = 0, a = 0; i < bufferLength; i++) {
+      const r = m.r(i);
+      d += `${m.props.x + Math.sin(a)*r},${m.props.y - Math.cos(a)*r} `;
+      a += sliceWidth;
+    }
+    d += "Z";
     return <path d={d} strokeWidth={1} stroke={"rgba(255, 255, 255, 0.4)"} fill="none"/>;
   }
 
@@ -93,21 +109,40 @@ class CanvasVisualizer extends Visualizer {
   componentDidMount() {
     setInterval(this.updateCanvas, 1000 / framerate);
   }
-
-  generateLine(v: (i: number) => number, y: (v: number) => number): void {
+  getCanvasContext(): CanvasRenderingContext2D|undefined {
     if (!this.ref) return;
     const ctx = this.ref.getContext("2d");
     if (!ctx) return;
-    const bufferLength = this.props.analyzer.frequencyBinCount;
     ctx.clearRect(0, 0, 960, 540);
     ctx.lineWidth = 1;
     ctx.strokeStyle = "rgba(255, 255, 255, 0.4)";
-    ctx.beginPath();
+    return ctx;
+  }
+
+  generateLine(m: LineModel): void {
+    const ctx = this.getCanvasContext();
+    if (!ctx) return;
+    const bufferLength = this.props.analyzer.frequencyBinCount;
     const sliceWidth = dim.w / bufferLength;
+    ctx.beginPath();
     for (let i = 0, x = 0; i < bufferLength; i++) {
-      ctx.lineTo(x, y(v(i)));
+      ctx.lineTo(x, m.y(i));
       x += sliceWidth;
     }
+    ctx.stroke();
+  }
+  generateCircle(m: CircleModel): void {
+    const ctx = this.getCanvasContext();
+    if (!ctx) return;
+    const bufferLength = this.props.analyzer.frequencyBinCount;
+    const sliceWidth = 2*Math.PI / bufferLength;
+    ctx.beginPath();
+    for (let i = 0, a = 0; i < bufferLength; i++) {
+      const r = m.r(i);
+      ctx.lineTo(m.props.x + Math.sin(a)*r, m.props.y - Math.cos(a)*r);
+      a += sliceWidth;
+    }
+    ctx.closePath();
     ctx.stroke();
   }
 
